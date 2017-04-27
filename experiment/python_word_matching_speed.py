755a05247109578e451d9421851da5ea26cae3fb
Karol <notkarol@gmail.com>
2017-04-27 03:49:26 -0400

    Fixed missing basename for saving file and added a plot

--- experiment/python_word_matching_speed.py
+++ experiment/python_word_matching_speed.py
@@ -6,10 +6,12 @@
 # Then prints out how long each function takes to find all matching words
 # Generates various hand sizes to see if there's any scaling
 
-import sys
-import timeit
+import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+import os
+import sys
+import timeit
 
 # Naive list way of matching wordbank
 def f0_list(hand, wordbank):
@@ -88,16 +90,15 @@ for w_i in range(len(words)):
 
 
 # Arrays for keeping track of functions and data-specific wordbanks
+hand_sizes = list(range(2, 9))
 functions = {'list' : [f0_list, f1_list],
              'numpy': [f0_np, f1_np, f2_np, f3_np, f4_np]}
 wordbanks = {'list' : wordbank_list,
              'numpy': wordbank_np}
-n_iter = 20
+n_iter = 10 if len(sys.argv) < 3 else int(sys.argv[2])
 timings = {}
 for datatype in functions:
-    timings[datatype] = {}
-    for hand_size in range(2, 9):
-        timings[datatype][hand_size] = np.zeros((n_iter, len(functions[datatype])))
+    timings[datatype] = np.zeros((max(hand_sizes) + 1, n_iter, len(functions[datatype])))
 
         
 # Verify that our functions give the same answers
@@ -109,9 +110,7 @@ for datatype in functions:
 # Time each word
 imports = 'from __main__ import functions, wordbanks'
 for counter in range(n_iter):
-    print("\r%.3f%%" % (100 * (counter + 1) / n_iter), end='')
-
-    for hand_size in range(2, 9):
+    for hand_size in hand_sizes:
 
         # Get a specific hand size
         hand = [13,3,3,6,18,3,4,3,12,2,2,5,3,8,11,3,2,9,6,9,6,3,3,2,3,2]
@@ -128,21 +127,25 @@ for counter in range(n_iter):
         for datatype in functions:
             for f_i in range(len(functions[datatype])):
                 cmd = 'functions["%s"][%i](%s, wordbanks["%s"])' % (datatype, f_i, hand, datatype)
-                timings[datatype][hand_size][counter, f_i] += timeit.timeit(cmd, imports, number=8)
+                timings[datatype][hand_size, counter, f_i] += timeit.timeit(cmd, imports, number=8)
+    print("\rCompleted %.1f%%" % (100 * (counter + 1) / n_iter), end='')
 print()
 
 
-# Save words and timings
-with open('word_matching_timings_%s.pkl' % sys.argv[1], 'wb') as f:
+# Save words and timings in case we're doing a long-lasting operation
+filename = 'word_matching_timings_%s.pkl' % os.path.basename(sys.argv[1])
+with open(filename, 'wb') as f:
+    print("Saving", filename)
     pickle.dump((words, wordbanks, timings), f)
 
     
 # Show Results
-print("Data\t#Hand\tF0\tF1\tF2\tF3\tF4")
 for datatype in functions:
-    for hand_size in range(2, 9):
-        print("%s\t%i" % (datatype, hand_size), end='')
-        means = np.mean(timings[datatype][hand_size], axis=0)
-        for f_i in range(len(functions[datatype])):
-            print("\t%7.5f" % (means[f_i]), end='')
-        print()
+    means = np.mean(timings[datatype], axis=1)
+    for f_i in range(means.shape[1]):
+        plt.semilogy(hand_sizes, means[:, f_i][min(hand_sizes):], label='%s F%i' % (datatype, f_i))
+plt.legend(loc='center left', bbox_to_anchor=(0.85, 0.5))
+plt.xlabel("Hand Size")
+plt.ylabel("Execution Time")
+plt.title("Word Matching")
+plt.show()
